463ee145d16aab669d54e2b05ad1a612980485f5
Balqist Retrianto <balqistretrianto@gmail.com>
2024-03-07 04:19:13 +0700

    Update simple_llm.py
    delete api key

--- simple_llm.py
+++ simple_llm.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 import gradio as gr
 
 # Memasukkan API key
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = ""
 
 # Mendefinisikan jenis model 
 gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )
